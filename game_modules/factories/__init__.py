64737c2f8952daba9835bcacaabf3a5738a0305c
Dragynrain <dragynrain@comcast.net>
2025-09-15 22:16:10 -0700

    Aborted phase 4

--- game_modules/factories/__init__.py
+++ game_modules/factories/__init__.py
@@ -1,8 +1,5 @@
 """Factory patterns for entity and object creation."""
 
 from .entity_factory import EntityFactory
-from .item_factory import ItemFactory
-from .level_factory import LevelFactory
-from .factory_registry import FactoryRegistry
 
-__all__ = ['EntityFactory', 'ItemFactory', 'LevelFactory', 'FactoryRegistry']
+__all__ = ['EntityFactory']
